238470b801b90473a8909a8cf9fff4bf13f127e2
Priyank Gupta <pryankg@amazon.com>
2023-06-03 15:06:45 -0700

    Initital UI test!

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hello WePloyo!
           </a>
         </header>
       </div>
